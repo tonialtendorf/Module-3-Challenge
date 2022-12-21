a106277d5c2f709ff13db5e62ac9e595045284f6
tonialtendorf <tvohnoutka05@gmail.com>
2022-12-21 10:40:21 -0600

    debugged code

--- script.js
+++ script.js
@@ -10,18 +10,56 @@ var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
 var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var confirmLength = "";
-var confirmSpecialCharacters;
-var confirmuppercaseCharacters;
-var confirmlowercaseCharacters;
-var confirmnumberCharacters;
 
-// Pop up prompts: length of password
+// Pop up text prompts: length of password
+//https://www.w3schools.com/js/js_loop_while.asp
 function generatePassword() {
-var confirmLength = (prompt("Confirm length of password between 8-128 characters."));
-}
- 
+  var confirmLength = (prompt("Confirm length of password between 8-128 characters."));
+  while(confirmLength < 8 || confirmLength > 128) {
+    alert("Password length must be between 8-128 characters.");  
+    var confirmLength = (prompt("Please reconfirm length of password between 8-128 characters."));
+  }
 
+// Pop up confirm prompts: selecting characters
+  var confirmSpecialCharacters = (confirm("Confirm if you would like to include special characters."));
+  var confirmuppercaseCharacters = (confirm("Confirm if you would like to include uppercase characters."));
+  var confirmlowercaseCharacters = (confirm("Confirm if you would like to include lowercase characters."));
+  var confirmnumberCharacters = (confirm("Confirm if you would like to include numerical characters."));
+
+// if no prompts are selected- reconfirm confirm pop ups
+    while(confirmSpecialCharacters === false && confirmuppercaseCharacters === false && confirmlowercaseCharacters === false && confirmnumberCharacters === false) {
+      alert("You must select at least one criteria to generate your password.");
+      var confirmSpecialCharacters = (confirm("Confirm if you would like to include special characters."));
+      var confirmuppercaseCharacters = (confirm("Confirm if you would like to include uppercase characters."));
+      var confirmlowercaseCharacters = (confirm("Confirm if you would like to include lowercase characters."));
+      var confirmnumberCharacters = (confirm("Confirm if you would like to include numerical characters."));
+  }
+
+//concat password
+var confirmPassword = []
+  if (confirmSpecialCharacters) {
+    confirmPassword = confirmPassword.concat(specialCharacters)
+  }
+  if (confirmuppercaseCharacters) {
+    confirmPassword = confirmPassword.concat(uppercaseCharacters)
+  }
+  if (confirmlowercaseCharacters) {
+    confirmPassword = confirmPassword.concat(lowercaseCharacters)
+  }
+  if (confirmnumberCharacters) {
+    confirmPassword = confirmPassword.concat(numberCharacters)
+  }
+
+
+//loop for final password
+//https://www.w3schools.com/js/js_random.asp
+  var finalPassword = ""
+  for (var i = 0; i < confirmLength; i++) {
+  finalPassword = finalPassword + confirmPassword[Math.floor(Math.random() * confirmPassword.length)];
+  }
+  // end function and return final value
+  return finalPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
